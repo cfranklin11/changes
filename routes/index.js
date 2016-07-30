3f67664840c69402add9f16f6004be84f27f0308
Craig Franklin <craigjfranklin@gmail.com>
2016-07-30 12:26:13 +1000

    add data route

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,16 @@
 
 var express = require('express');
 var router = express.Router();
+var dataHelper = require('../lib/data-helper');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+router.get('/data',
+  dataHelper.getData,
+  function(req, res, next) {
+  res.render('index', { title: 'Data' });
 });
 
 module.exports = router;

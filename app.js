0ccb72adc6bcdcfc4902b5e54fd9ff558d381119
Craig Franklin <craigjfranklin@gmail.com>
2016-07-30 11:18:37 +1000

    add dataHelper

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+'use strict';
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var auth = require('./config/auth');
 
 var routes = require('./routes/index');
 
---
+++ config/auth.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var fs = require('fs');
+var env = {};
+var envFile = __dirname + '/env.json';
+
+if (fs.existsSync(envFile)) {
+  env = fs.readFileSync(envFile, 'utf-8');
+  env = JSON.parse(env);
+  Object.keys(env).forEach(function(key) {
+    process.env[key] = env[key];
+  });
+}
+
+module.exports = {
+
+};

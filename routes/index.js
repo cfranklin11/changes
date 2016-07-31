ee50d7a7e02c8340359e07f32fae0a6a547311b0
Craig Franklin <craigjfranklin@gmail.com>
2016-07-31 15:03:42 +1000

    cleanup

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,7 @@ var dataHelper = require('../lib/data-helper');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'My Climate Change', categories: ['temperature', 'precipitation'] });
-});
-router.get('/data', function(req, res, next) {
-  res.render('data', {title: 'Data'});
+  res.render('index', {title: 'Ch-ch-ch-changes'});
 });
 router.get('/api/data',
   dataHelper.getData,
